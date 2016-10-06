538121e8d702241d734d7659f7696ba77b2e3da4
Rui Chicória <rchicoria@Ruis-MBP-2.lan>
2016-10-06 16:38:50 +0100

    Dropped auto save and added mqtt subscribes and publish

--- mictor.io/imports/api/pees.js
+++
@@ -1,9 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const Pees = new Mongo.Collection('pees');
-
-Pees.before.insert(function (userId, doc) {
-  doc.createdAt = Date.now();
-});
-
-Pees.mqttConnect("mqtt://localhost", ["finish"], {insert: true}, {});
--- mictor.io/server/main.js
+++ mictor.io/server/main.js
@@ -1,6 +1,25 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/api/pees.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  const Pees = new Mongo.Collection('pees');
+
+  Pees.before.insert(function (userId, doc) {
+    doc.createdAt = Date.now();
+  });
+
+  const mqtt = require('mqtt');
+  const MQTTClient = mqtt.connect('mqtt://localhost');
+
+  MQTTClient.on('connect', () => {
+    MQTTClient.subscribe('mictor-io.start');
+    MQTTClient.subscribe('mictor-io.end');
+  });
+
+  MQTTClient.on('message', Meteor.bindEnvironment(function(topic, message){
+      if(topic === 'mictor-io.start') {
+      } else if(topic === 'mictor-io.end') {
+          Pees.insert(JSON.parse(message.toString()));
+      }
+    })
+  );
 });
